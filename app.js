06f8e8a28781bac829b6621bb7dd38196d86bcf9
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-05-05 21:14:31 +0800

    GET學生資料釋例

--- app.js
+++ app.js
@@ -14,6 +14,50 @@ mongoose
     console.log(e);
   });
 
+// 獲得所有學生的資料
+app.get("/students", async (req, res) => {
+  try {
+    let studentData = await Student.find({}).exec();
+    return res.send(studentData);
+  } catch (e) {
+    return res.status(500).send("尋找資料時發生錯誤...");
+  }
+});
+
+// 獲得特定的學生資料
+app.get("/students/:id", async (req, res) => {
+  let { id } = req.params;
+  try {
+    let foundStudent = await Student.findOne({
+      _id: id,
+    }).exec();
+    return res.send(foundStudent);
+  } catch (e) {
+    return res.status(500).send("尋找資料時發生錯誤...");
+  }
+});
+
+// 獲得特定的學生資料(修改，使程式碼簡潔一點)
+app.get("/students/:_id", async (req, res) => {
+  let { _id } = req.params;
+  try {
+    let foundStudent = await Student.findOne({ _id }).exec();
+    return res.send(foundStudent);
+  } catch (e) {
+    return res.status(500).send("尋找資料時發生錯誤...");
+  }
+});
+
+// 找資料測試
+Student.findOne({ _id: "6628840806770b88f5117b7b" })
+  .exec()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 app.listen(3000, () => {
   console.log("伺服器正在聆聽port 3000");
 });
